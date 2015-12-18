b0c252f86d45fb48d42e2c74f58839dc7c831c34
Philipp Roscher <s2740467@mail.zih.tu-dresden.de>
2015-12-18 14:59:33 +0100

    removed unnecessary imports

--- Prototypen/Prototype/core/src/org/sausagepan/prototyp/managers/OneSecondSystem.java
+++ Prototypen/Prototype/core/src/org/sausagepan/prototyp/managers/OneSecondSystem.java
@@ -1,26 +1,7 @@
 package org.sausagepan.prototyp.managers;
 
 import org.sausagepan.prototyp.KPMIPrototype;
-import org.sausagepan.prototyp.model.Maze;
-import org.sausagepan.prototyp.model.components.InventoryComponent;
-import org.sausagepan.prototyp.model.components.TeamComponent;
-import org.sausagepan.prototyp.model.components.WeaponComponent;
-import org.sausagepan.prototyp.model.entities.CharacterEntity;
-import org.sausagepan.prototyp.model.entities.EntityFamilies;
-import org.sausagepan.prototyp.model.items.Bow;
-
-import com.badlogic.ashley.core.ComponentMapper;
-import com.badlogic.ashley.core.Engine;
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.core.EntityListener;
-import com.badlogic.ashley.core.EntitySystem;
-import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IntervalSystem;
-import com.badlogic.ashley.utils.ImmutableArray;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.MapObjects;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
-import com.badlogic.gdx.math.Rectangle;
 
 /**
  * Created by philipp on 18.12.2015.
