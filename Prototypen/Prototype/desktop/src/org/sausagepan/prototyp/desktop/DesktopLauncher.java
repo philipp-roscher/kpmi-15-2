1be7cf37e7cca2e00304ac6c2f949435090cdece
Philipp Roscher <s2740467@mail.zih.tu-dresden.de>
2016-01-27 00:39:37 +0100

    renamed app

--- Prototypen/Prototype/desktop/src/org/sausagepan/prototyp/desktop/DesktopLauncher.java
+++ Prototypen/Prototype/desktop/src/org/sausagepan/prototyp/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
-		config.title = "Prototype";
+		config.title = "Maze";
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new KPMIPrototype(), config);
